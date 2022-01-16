f649f7087cfa069d9a63290298502bf2f174e384
hooope81 <hooope81@gmail.com>
2022-01-16 19:08:11 +0300

    lesson1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+
+import './App.scss';
+import Message from './Message';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+     
+      <Message messageText={'Hello, word'}/>
+    
     </div>
   );
 }
-
 export default App;
+
+
+
+
---
+++ src/Message.js
@@ -0,0 +1,12 @@
+import './App.scss';
+
+
+function Message({messageText}) {
+
+    return (
+
+        <h3>{messageText}!!!</h3>
+
+    );
+}
+export default Message;
