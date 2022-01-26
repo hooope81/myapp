e2a3940a8ee507f11c1fd934f0c941133657da61
hooope81 <hooope81@gmail.com>
2022-01-27 00:17:33 +0300

    les2

--- src/App.js
+++ src/App.js
@@ -1,21 +1,52 @@
-
+import React, { useEffect, useState } from 'react';
 import './App.scss';
-import Message from './Message';
+
 
 
 
 function App() {
-  return (
-    <div className="App">
-      
-     
-      <Message messageText={'Hello, word'}/>
-    
-    </div>
-  );
-}
-export default App;
+  const [value, setValue] = useState('');
+  const [messageList, setMessageList] = useState([]);
 
+  const handleCnange = (event) => {
+    setValue(event.target.value);
+  }
+
+  function add() {
+    setMessageList([...messageList, { text: value, author: "me" }]);
+    setValue('');
+  };
+
+  const result = messageList.map((element, index) => {
+    return <div className='wrap' key={index}><span className='author'>{element.author} </span><p className='chat' >{element.text} </p></div>;
+  });
+
+  useEffect(() => {
+    if (messageList.length > 0 && messageList[messageList.length - 1].author === 'me') {
+      setTimeout(() => {
+        setMessageList([...messageList, { text: 'Hello!', author: "bot" }]);
+      }, 1500);
+    }
+  }, [messageList]);
+
+  return (
+    <div className='box'>
+      <h1 className='heading'>Chat</h1>
+      <div className='App'>
+
+        {result}
+      </div>
+      <div className='mess'>
+        <input className='input' type="text" value={value} onChange={handleCnange} placeholder='Написать сообщение...'></input>
+        <button className='btn' onClick={add}>
+          &#10148;</button>
+      </div>
+
+    </div>
+  )
+}
+
+export default App;
 
 
 
