73e3b994d8e525cfe538c7d04e7cf750e87f03b1
hooope81 <hooope81@gmail.com>
2022-02-02 22:26:37 +0300

    lesson5

--- src/page/profile.js
+++ src/page/profile.js
@@ -1,6 +1,30 @@
+
+import {useCallback} from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import {toggleShowName} from '../store/profile/chatActions';
+
 const Profile = () => {
+   
+    const {name, showName} = useSelector(state => state);
+const dispatch = useDispatch();
+    const setShowName = useCallback(() => {
+        dispatch(toggleShowName);
+       
+    }, [dispatch])
+    
     return (
-        <div>Profile</div>
+        <div>
+         <h4>Profile</h4>
+         <input 
+           type="checkbox"
+           checked={showName}
+           value={showName}
+           onChange={setShowName}
+        />
+        <span>Show Name</span>
+
+        {showName && <div>{name}</div>}
+        </div>
     );
 };
 
